8228fa899c7b8108e1070d8fc9b6aab57b72a072
Japplet <juantriana@seti.com.co>
2022-09-21 08:46:22 -0500

    :tada: Creamos el modulo de admin-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { AntennaComponent } from './screens/antenna/antenna.component';
+import { AdminProfileComponent } from './screens/admin-profile/admin-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AntennaComponent,
+    AdminProfileComponent,
   ],
   imports: [
     BrowserModule,
